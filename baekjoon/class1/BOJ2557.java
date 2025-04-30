f2f24655818bd9108fc125c7247f4430c6755925
eziquexx <waftyann@gmail.com>
2025-04-30 23:42:36 +0900

    [FIX] BOJ2557 - Hello World : 주석 추가, class 파일 삭제

--- baekjoon/class1/BOJ2557.java
+++ baekjoon/class1/BOJ2557.java
@@ -1,5 +1,8 @@
 package baekjoon.class1;
 
+// 문제 푼 날짜: 25.04.30
+// 문제 번호: BOJ 2557
+// 문제 제목: Hello World
 public class BOJ2557 {
   public static void main(String[] args) {
     System.out.println("Hello World!");
