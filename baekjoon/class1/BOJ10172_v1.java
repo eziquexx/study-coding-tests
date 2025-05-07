922e17f30e8704484872c8522f2598c73532adb6
eziquexx <waftyann@gmail.com>
2025-05-08 00:27:51 +0900

    [BOJ] 10172 : 개

---
+++ baekjoon/class1/BOJ10172_v1.java
@@ -0,0 +1,17 @@
+package baekjoon.class1;
+
+// 문제 푼 날짜: 25.05.08
+// 문제 번호: BOJ 10172
+// 문제 제목: 개
+// 풀이 내용: 
+// (1) 한 줄씩 System.out.println(); 으로 출력하기
+// (2) 역슬래시와 쌍따옴표를 사용하려면 역슬래시하고 아스키코드 문자로 작성해야한다.
+public class BOJ10172_v1 {
+  public static void main(String[] args) {
+    System.out.println("|\\_/|");
+    System.out.println("|q p|   /}");
+    System.out.println("( 0 )\"\"\"\\");
+    System.out.println("|\"^\"`    |");
+    System.out.println("||_/=\\\\__|");
+  }
+}
