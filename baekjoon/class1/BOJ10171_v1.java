cea327fbd3a89fd2e90f674eed292e3ec82aa313
eziquexx <waftyann@gmail.com>
2025-05-08 00:17:37 +0900

    [BOJ] 10171 : 고양이

---
+++ baekjoon/class1/BOJ10171_v1.java
@@ -0,0 +1,17 @@
+package baekjoon.class1;
+
+// 문제 푼 날짜: 25.05.08
+// 문제 번호: BOJ 10171
+// 문제 제목: 고양이
+// 풀이 내용: 
+// (1) 한 줄씩 System.out.println(); 으로 출력하기
+// (2) \ 역슬래시를 출력하기 위해서는 2개를 연달아 작성해야한다.
+//     이유는 1개만 사용하면 이스케이프 문자를 뜻하기 때문이다.
+public class BOJ10171_v1 {
+  public static void main(String[] args) {
+    System.out.println("\\    /\\");
+    System.out.println(" )  ( ')");
+    System.out.println("(  /  )");
+    System.out.println(" \\(__)|");
+  }
+}
