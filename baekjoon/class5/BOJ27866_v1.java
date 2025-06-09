fa24798feb7798895a778319f7ca04855d6b969a
eziquexx <waftyann@gmail.com>
2025-06-09 15:11:58 +0900

    [BOJ] 27866 : 풀이 내용 수정

--- baekjoon/class5/BOJ27866_v1.java
+++ baekjoon/class5/BOJ27866_v1.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 // 문제 푼 날짜: 25.06.09
 // 문제 번호: BOJ 27866
 // 풀이 내용: 
-// (1) BufferedReader + InputStreamReader 클래스를 사용하여 입력값 받기.
+// (1) BufferedReader 클래스를 사용하여 입력값 받기.
 // (2) String S에 단어 입력 받기.
 // (3) int i에 몇 번째 글자 출력하지 숫자 입력 받기.
 // (4) char[] letters에 toCharArray() 사용해서 한 글자씩 배열에 담기.
