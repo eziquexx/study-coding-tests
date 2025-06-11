be17a81896851f78769bda2fedda462c5081ae7e
eziquexx <waftyann@gmail.com>
2025-06-11 23:53:42 +0900

    [BOJ] 11720 : 풀이 내용 수정

--- baekjoon/class5/BOJ11720_v1.java
+++ baekjoon/class5/BOJ11720_v1.java
@@ -8,8 +8,13 @@ import java.io.InputStreamReader;
 // 문제 번호: BOJ 11720
 // 풀이 내용: 
 // (1) BufferedReader 클래스를 사용하여 입력값 받기.
-// (2) String input에 입력값 받아서 저장.
-// (3) 출력시 input.hashCode() 를 사용하여 아스키코드로 출력.
+// (2) int N에 몇 개 받을지 저장.
+// (3) int[] nums에 N개 크기로 생성.
+// (4) String[] input에 입력받은 숫자들을 1글자씩 배열로 담기.
+// (5) int sum 합계 담는 변수 선언.
+// (6) for문 사용해서 입력 받은 input값을 nums에 배열로 저장.
+// (6) for-each문 사용해서 nums에 있는 값 전체 더하기.
+// (7) 총 합계 출력.
 public class BOJ11720_v1 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
