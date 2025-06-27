395db9bbffb421fdb844496c9d24143f3ccc9a83
eziquexx <waftyann@gmail.com>
2025-06-27 23:35:31 +0900

    [BOJ] 2745 : 진법 변환 과정 주석으로 추가

--- baekjoon/class8/BOJ2745_1.java
+++ baekjoon/class8/BOJ2745_1.java
@@ -4,6 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 문제 푼 날짜: 25.06.27
+// 문제 번호: BOJ 2745
+// 풀이 내용: 
+// 1. BufferedReader로 입력값 받기.
+// 2. String[] input에 입력값을 공백으로 나누어 저장.
+// 3. String B와 int N에 input[0], input[1] 값 저장.
+// 4. int[] az에 알파벳 개수 크기로 생성.
+// 5. int x = 10 선언 및 초기화. 나중에 필요.
+// 6. for문을 사용하여 문제에 출시된 A=10, B=11, ... , Z=35 설정하기 위해 az 배열에 순차적으로 x+=1 값을 저장.
+// 7. for문을 사용하여 10진수 인지 아닌지 체크를 위해 B의 한 글자씩 0~9의 숫자인지 비교하기.
+//    : 만약에 숫자면 azIndex[i] 위치에 c를 저장하고,
+//    : 숫자가 아니라면 알파벳 자리 계산하여 index에 저장하고 
+//    : az[] 배열이 알파벳 순으로 값을 저장해 놓은 것이므로,
+//    : azIndex[i]에 az[index] 값을 저장
+// 8. azIndex에 담긴 값을 N진법으로 계산을 중첩 for문으로 계산
+//    : 예를들어, 5개의 문자로 이루어진거라면 0번째는 4번, 1번째는 3번, 2번째는 2번, 3번째는 1번, 4번째는 0번
+//    : 이러한 순으로 계산해야하기 때문에 바깥 for문은 int i = 0; input[0].lenght 만큼 반복.
+//    : 안쪽 for문은 int j = input[0].length - 1 값으로 해야하는데,
+//    : count에 input[0].length을 저장하고, int j = count - 1; 로 지정하여 0보다 작을때까지 j-- 로 반복
+//    : 그리고 count-- 하여 4, 3, 2, 1 반복, 3, 2, 1 반복할 수 있도록 설정.
+//    : 반복할 때마다 multiply = multiply * N 을 저장. (N은 진수법)
+//    : 안쪽 for문 끝날때마다 result에 result + (multiply * azIndex[i]) 값을 누적시켜준다.
+// 9. 마지막에 result 출력. 그리고 BufferedReader .close() 해주기
+
 public class BOJ2745_1 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
